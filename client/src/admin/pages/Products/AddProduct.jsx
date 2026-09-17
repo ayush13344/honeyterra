435a676f3e68c7c5f2907bbb7013b29f1c0914c5
ayush13344 <147728663+ayush13344@users.noreply.github.com>
2026-09-17 23:48:57 +0530

    a

--- client/src/admin/pages/Products/AddProduct.jsx
+++ client/src/admin/pages/Products/AddProduct.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "./products.css";
+import "./Products.css";
 
 const AddProduct = () => {
   // ==========================================
